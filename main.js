a02db59900886cf6cde5c8b53807b6ecdb2c2254
LeoX DEV <84378319+Le0X8@users.noreply.github.com>
2023-04-23 21:24:52 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,125 @@
+const Hssp = require('hssp');
+const tar = require('tar');
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
+const bplist = require('bplist-parser');
+
+
+
+const tvOSversion = 16; // must be 10 or higher
+
+
+
+if (tvOSversion < 10) return;
+
+const get = (url) => new Promise((resolve, reject) => (url.startsWith('https://') ? https : http).get(url, { rejectUnauthorized: false }, (response) => {
+    var dlTarget = Buffer.alloc(0);
+
+    response.on('data', (chnk) => dlTarget = Buffer.concat([dlTarget, chnk]));
+    response.on('end', () => resolve(dlTarget));
+    response.on('error', reject);
+}));
+
+const getDirsInDir = (dirPath, rmInName) => {
+    dirPath = dirPath.endsWith('/') ? dirPath.slice(0, -1) : dirPath;
+    const files = fs.readdirSync(dirPath);
+    const result = [];
+
+    files.forEach((file) => {
+        const filePath = `${dirPath}/${file}`;
+        const stat = fs.statSync(filePath);
+        if (stat.isDirectory()) {
+            result.push(filePath);
+            result.push(...getDirsInDir(filePath));
+        };
+    });
+
+    if (rmInName) result.forEach((res, i) => result[i] = res.replace(dirPath + '/', ''));
+
+    return result;
+};
+
+const getFilesInDir = (dirPath, rmInName) => {
+    dirPath = dirPath.endsWith('/') ? dirPath.slice(0, -1) : dirPath;
+    const files = fs.readdirSync(dirPath);
+    const result = [];
+
+    files.forEach((file) => {
+        const filePath = `${dirPath}/${file}`;
+        const stat = fs.statSync(filePath);
+
+        if (stat.isDirectory()) {
+            result.push(...getFilesInDir(filePath));
+        } else {
+            result.push(filePath);
+        };
+    });
+
+    if (rmInName) result.forEach((res, i) => result[i] = res.replace(dirPath + '/', ''));
+
+    return result;
+};
+
+fs.mkdirSync('./temp');
+fs.mkdirSync('./temp/extracted');
+
+const resourceUrl = {
+    '16': 'https://sylvan.apple.com/Aerials/resources-16.tar',
+    '15': 'https://sylvan.apple.com/Aerials/resources-15.tar',
+    '14': 'https://sylvan.apple.com/Aerials/resources-14.tar',
+    '13': 'https://sylvan.apple.com/Aerials/resources-13.tar',
+
+    '12': 'https://sylvan.apple.com/Aerials/resources.tar',
+
+    '11': 'https://sylvan.apple.com/Aerials/2x/entries.json',
+
+    '10': 'http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/entries.json'
+}[tvOSversion.toString()];
+
+(async () => {
+    if (tvOSversion > 11) {
+        fs.writeFileSync('./temp/resources.tar', await get(resourceUrl));
+
+        await new Promise((resolve, reject) => fs.createReadStream('./temp/resources.tar').pipe((() => {
+            const extract = tar.extract({ cwd: './temp/extracted' });
+            extract.on('finish', () => resolve());
+            return extract;
+        })()));
+
+        fs.rmSync('./temp/resources.tar');
+    } else {
+        fs.writeFileSync('./temp/extracted/entries.json', await get(resourceUrl));
+    };
+
+    const resources = getFilesInDir('./temp/extracted', true);
+    const resourcesDirs = getDirsInDir('./temp/extracted', true);
+
+    const hsspArchive = new Hssp();
+    hsspArchive.addFolder('strings');
+    hsspArchive.addFile('resources.json', Buffer.from(
+        JSON.stringify(
+            JSON.parse(
+                fs.readFileSync('./temp/extracted/entries.json').toString('utf8')
+            )
+        )
+    ), true);
+
+    for (var i = 0; i < resources.length; i++) {
+        if (resources[i].endsWith('.lproj/Localizable.nocache.strings')) hsspArchive.addFile('strings/' + resources[i].split('.')[1].split('/')[1], Buffer.from(
+            JSON.stringify(
+                bplist.parseBuffer(
+                    fs.readFileSync('./temp/extracted/' + resources[i])
+                )
+            )
+        ));
+        fs.rmSync('./temp/extracted/' + resources[i]);
+    };
+
+    for (var i = resourcesDirs.length - 1; i > -1; i--) fs.rmdirSync('./temp/extracted/' + resourcesDirs[i]);
+
+    fs.rmdirSync('./temp/extracted');
+    fs.rmdirSync('./temp');
+
+    fs.writeFileSync('./resources.hssp', hsspArchive.toBuffer());
+})();
